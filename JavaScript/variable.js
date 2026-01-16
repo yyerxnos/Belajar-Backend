90dbbee18bb3d2ebc3b04957e651be3ca7bd52ab
SnoxReyy <radtyatransport@gmail.com>
2026-01-16 21:04:25 +0800

    const gak bisa diubah

--- JavaScript/variable.js
+++ JavaScript/variable.js
@@ -1,7 +1,15 @@
-let username = 'radit';
+//let username = 'radit';
 
-console.log('sebelum diubah:', username); // output:Sebelum diubah: Radit
+//console.log('sebelum diubah:', username); // output:Sebelum diubah: Radit
 
-username = 'raditya';
+//username = 'raditya';
 
-console.log('Setelah diubah:', username); // output: Setelah diubah :Raditya
+//console.log('Setelah diubah:', username); // output: Setelah diubah :Raditya
+
+const username = 'radit';
+
+console.log('sebelum diubah:', username); // output: sebelum diubah: radit
+
+username = 'raditya'; // TypeError: Assignment to constant variable.
+
+console.log('setelah diubah:', username); // tidak akan di eksekusi
